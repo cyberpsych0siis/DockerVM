693ed9e02031956fa492bee285415dc271b5414e
Philipp Gaßner <activator112233@gmail.com>
2022-03-06 11:41:07 +0100

    Update VsCodeTraefikProvider.js

--- provider/VsCodeTraefikProvider.js
+++ provider/VsCodeTraefikProvider.js
@@ -9,14 +9,16 @@ export default class VsCodeTraefikProvider extends LabelProvider {
      * @returns Object
      */
     getProperties(containerName, reachableAddress) {
+        const uuid = reachableAddress.split(".")[0];
         return {
             Image: "cyberpsych0siis/code-server",
             //Cmd: ["/opt/code-server", "--port", "8080"],
             Labels: {
                 "traefik.enable": "true",
                 "traefik.port": "8080",
+                ["traefik.http.routers." + containerName + ".middlewares"]: "strip_then_auth@file",
                 ["traefik.http.routers." + containerName + ".entrypoints"]: "web",
-                ["traefik.http.routers." + containerName + ".rule"]: "Host(`" + reachableAddress + "`)"
+                ["traefik.http.routers." + containerName + ".rule"]: "Host(`" + reachableAddress + "`) || PathPrefix(`/" + uuid + "`)"
             },
         }
     }
