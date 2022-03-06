60883df6b1e91bc2cf74a053348209f730369a62
Philipp Gaßner <activator112233@gmail.com>
2022-03-06 09:31:45 +0100

    Update VsCodeTraefikProvider.js

--- provider/VsCodeTraefikProvider.js
+++ provider/VsCodeTraefikProvider.js
@@ -10,7 +10,7 @@ export default class VsCodeTraefikProvider extends LabelProvider {
      */
     getProperties(containerName, reachableAddress) {
         return {
-            Image: "chanchal/vscode-server",
+            Image: "vyberpsych0siis/code-server",
             //Cmd: ["/opt/code-server", "--port", "8080"],
             Labels: {
                 "traefik.enable": "true",
