4bbb038f56ec554f84cc3a131a45ad268151b3c8
Philipp Gaßner <thallosaurus>
2022-03-15 07:12:38 +0100

    modularity

--- provider/RdpTraefikProvider.js
+++ provider/RdpTraefikProvider.js
@@ -6,29 +6,32 @@
       - traefik.tcp.services.xrdp.loadbalancer.server.port=3389
  */
 
-import { LabelProvider } from "../DockerClient.js";
+import { LabelProvider } from "./LabelProvider.js";
 
-export default class RdpTraefikProvider extends LabelProvider {
-    /**
-     * 
-     * @param {string} containerName 
-     * @param {string} reachableAddress 
-     * @param {string} proxyPort 
-     * @returns Object
-     */
-    getProperties(containerName, reachableAddress) {
-        return {
-            Hostname: containerName,
-            Image: "frxyt/xrdp:xfce",
-            Labels: {
-                "traefik.enable": "true",
-                "traefik.port": "3389",
-                ["traefik.tcp.routers." + containerName + ".entrypoints"]: "rdp",
-                ["traefik.tcp.routers." + containerName + ".entrypoints.tls"]: "{}",
-                ["traefik.tcp.routers." + containerName + ".tls.certresolver"]: "sslresolver",
-                ["traefik.tcp.routers." + containerName + ".rule"]: "HostSNI(`" + reachableAddress + "`)",
-                ["traefik.tcp.services." + containerName + ".loadbalancer.server.port"]: "3389"
-            },
-        }
-    }
+export class RdpTraefikProvider extends LabelProvider {
+  /**
+   *
+   * @param {string} containerName
+   * @param {string} reachableAddress
+   * @param {string} proxyPort
+   * @returns Object
+   */
+  getProperties(containerName, reachableAddress) {
+    return {
+      Hostname: containerName,
+      Image: "frxyt/xrdp:xfce",
+      Labels: {
+        "traefik.enable": "true",
+        "traefik.port": "3389",
+        ["traefik.tcp.routers." + containerName + ".entrypoints"]: "rdp",
+        ["traefik.tcp.routers." + containerName + ".entrypoints.tls"]: "{}",
+        ["traefik.tcp.routers." + containerName + ".tls.certresolver"]:
+          "sslresolver",
+        ["traefik.tcp.routers." + containerName + ".rule"]:
+          "HostSNI(`" + reachableAddress + "`)",
+        ["traefik.tcp.services." + containerName + ".loadbalancer.server.port"]:
+          "3389",
+      },
+    };
+  }
 }
--- provider/VsCodeTraefikProvider.js
+++ provider/VsCodeTraefikProvider.js
@@ -1,26 +1,27 @@
-import { LabelProvider } from "../DockerClient.js";
-
-export default class VsCodeTraefikProvider extends LabelProvider {
-    private = true;
-    /**
-     * 
-     * @param {string} containerName 
-     * @param {string} reachableAddress 
-     * @param {string} proxyPort 
-     * @returns Object
-     */
-    getProperties(containerName, reachableAddress) {
-        const uuid = reachableAddress.split(".")[0];
-        return {
-            Image: "thallosaurus.de/cyberpsych0siis/code-server",
-            //Cmd: ["/opt/code-server", "--port", "8080"],
-            Labels: {
-                "traefik.enable": "true",
-                "traefik.port": "8080",
-                ["traefik.http.routers." + containerName + ".middlewares"]: "auth_then_strip@file,errorcats@docker",
-                ["traefik.http.routers." + containerName + ".entrypoints"]: "web",
-                ["traefik.http.routers." + containerName + ".rule"]: "Host(`" + reachableAddress + "`) || Path(`/" + uuid + "`)"
-            },
-        }
-    }
+import { LabelProvider } from "./LabelProvider.js";
+export class VsCodeTraefikProvider extends LabelProvider {
+  private = true;
+  /**
+   *
+   * @param {string} containerName
+   * @param {string} reachableAddress
+   * @param {string} proxyPort
+   * @returns Object
+   */
+  getProperties(containerName, reachableAddress) {
+    const uuid = reachableAddress.split(".")[0];
+    return {
+      Image: "thallosaurus.de/cyberpsych0siis/code-server",
+      //Cmd: ["/opt/code-server", "--port", "8080"],
+      Labels: {
+        "traefik.enable": "true",
+        "traefik.port": "8080",
+        ["traefik.http.routers." + containerName + ".middlewares"]:
+          "auth_then_strip@file,errorcats@docker",
+        ["traefik.http.routers." + containerName + ".entrypoints"]: "web",
+        ["traefik.http.routers." + containerName + ".rule"]:
+          "Host(`" + reachableAddress + "`) || Path(`/" + uuid + "`)",
+      },
+    };
+  }
 }
