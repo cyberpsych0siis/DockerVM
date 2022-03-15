9c438779de9e0fa5ab34489187817b0c0fc9a699
Philipp Gaßner <thallosaurus>
2022-03-15 16:52:10 +0100

    dont mind me

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-
-import logger from 'morgan';
-import WebSocketRoute from './routes/WebSocketRoute.js';
-import HealthcheckRoute from './routes/HealthcheckRoute.js';
-import express from 'express';
-import process from 'process';
+import logger from "morgan";
+import WebSocketRoute from "./routes/WebSocketRoute.js";
+import HealthcheckRoute from "./routes/HealthcheckRoute.js";
+import express from "express";
+import process from "process";
 
 (function () {
-    const app = express();
-    app.use(logger('dev', {
-        skip: function (req, res) {
-            if (req.url == '/health' || req.url == '/') {
-                return true;
-            } else {
-                return false;
-            }
+  const app = express();
+  app.use(
+    logger("dev", {
+      skip: function (req, res) {
+        if (req.url == "/health" || req.url == "/") {
+          return true;
+        } else {
+          return false;
         }
-    }));
+      },
+    })
+  );
 
-    app.use("/health", HealthcheckRoute);
+  app.use("/health", HealthcheckRoute);
 
-    //our http server which handles websocket proxy and static
-    const PORT = process.env.WEBSOCKET_PORT ?? 8085;
-    const server = app.listen(PORT, () => { console.log("[WebSocket] Listening to port " + PORT) });/*  */
+  app.get("/_logs", (req, res) => {
+    res.send("Digga ich fress nen Besen wenn das jetzt angezeigt wird");
+  });
 
-    WebSocketRoute(server, app);
+  //our http server which handles websocket proxy and static
+  const PORT = process.env.WEBSOCKET_PORT ?? 8085;
+  const server = app.listen(PORT, () => {
+    console.log("[WebSocket] Listening to port " + PORT);
+  }); /*  */
+
+  WebSocketRoute(server, app);
 })();
